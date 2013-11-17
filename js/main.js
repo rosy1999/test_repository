384559c897d5bdbf1f3f30c62c6486dd8733ae79
Ros Bennis <rosbennis@gmail.com>
2013-11-17 20:20:13 +1100

    removing debugging

--- js/main.js
+++ js/main.js
@@ -4,11 +4,9 @@ function checkEnquiryType() {
 	
 	// if not product complaint, then submit the form
 	if(enquiryTypeValue != 'productComplaint') {
-		console.log("No Complaints Here");
 		return true;
 	}
 	else {
-		console.log("Complaint being Made");
 		// get the Product Name, Size, Use By and Batch Code values
 		var name = document.testForm.productName.value;
 		var size = document.testForm.productSize.value;
@@ -17,11 +15,9 @@ function checkEnquiryType() {
 		
 		// ensure all are filled in on the form before it is submitted. If not alert the user
 		if(name != '' && size != '' && useByDate != '' && batchCode != '') {
-			console.log("Nothing is Empty");
 			return true;
 		}
 		else {
-			console.log("Something not filled out");
 			alert("As you are lodging a Product Complaint, please ensure:\n\nProduct Name\nProduct Size\nUse By Date\nBatch Code\n\n are filled out before submitting your issue. Thank You");
 			return false;
 		}		
